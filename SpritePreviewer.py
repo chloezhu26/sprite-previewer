b57ca940a75ccc9cc77a2b0edba074ef297077bb
Wuyou Zhu <u1593592@umail.utah.edu>
2026-04-10 03:51:36 -0600

    finish assignment

--- SpritePreviewer.py
+++ SpritePreviewer.py
@@ -1,59 +1,63 @@
+import sys
 import math
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFrame, QVBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QTimer
 
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
 
-# This function loads a series of sprite images stored in a folder with a
-# consistent naming pattern: sprite_# or sprite_##. It returns a list of the images.
 def load_sprite(sprite_folder_name, number_of_frames):
     frames = []
-    padding = math.ceil(math.log(number_of_frames - 1, 10))
+    padding = math.ceil(math.log(number_of_frames - 1, 10)) if number_of_frames > 1 else 1
+
     for frame in range(number_of_frames):
-        folder_and_file_name = sprite_folder_name + "/sprite_" + str(frame).rjust(padding, '0') + ".png"
-        frames.append(QPixmap(folder_and_file_name))
+        file_name = sprite_folder_name + "/sprite_" + str(frame).rjust(padding, '0') + ".png"
+        frames.append(QPixmap(file_name))
 
     return frames
 
-class SpritePreview(QMainWindow):
 
+class SpritePreview(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.setWindowTitle("Sprite Animation Preview")
-        # This loads the provided sprite and would need to be changed for your own.
+
         self.num_frames = 21
-        self.frames = load_sprite('spriteImages',self.num_frames)
+        self.frames = load_sprite("spriteImages", self.num_frames)
 
-        # Add any other instance variables needed to track information as the program
-        # runs here
+        self.current_frame = 0
 
-        # Make the GUI in the setupUI method
         self.setupUI()
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(100)
 
 
     def setupUI(self):
-        # An application needs a central widget - often a QFrame
         frame = QFrame()
 
-        # Add a lot of code here to make layouts, more QFrame or QWidgets, and
-        # the other components of the program.
-        # Create needed connections between the UI components and slot methods
-        # you define in this class.
+        self.label = QLabel()
+        self.label.setPixmap(self.frames[0])
 
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+
+        frame.setLayout(layout)
         self.setCentralWidget(frame)
 
 
-    # You will need methods in the class to act as slots to connect to signals
+    def update_frame(self):
+        self.current_frame += 1
 
+        if self.current_frame >= self.num_frames:
+            self.current_frame = 0
 
-def main():
-    app = QApplication([])
-    # Create our custom application
-    window = SpritePreview()
-    # And show it
-    window.show()
-    app.exec()
+        self.label.setPixmap(self.frames[self.current_frame])
 
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    window = SpritePreview()
+    window.show()
+    sys.exit(app.exec_())
